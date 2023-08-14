743b1d7bf42762f962418c3703079c61b378d4b9
qcp3r <92098432+qcp3r@users.noreply.github.com>
2023-08-14 15:01:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const generateBtn = document.getElementById('generateBtn');
+const copyBtn = document.getElementById('copyBtn');
+const passwordDiv = document.getElementById('password');
+const lenghtInput = document.getElementById('lenghtInput');
+
+
+
+generateBtn.addEventListener('click', generatePassword);
+copyBtn.addEventListener('click', copyPassword);
+
+
+function CustomAlert(){
+    this.alert = function(message,title){
+      document.body.innerHTML = document.body.innerHTML + '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
+  
+      let dialogoverlay = document.getElementById('dialogoverlay');
+      let dialogbox = document.getElementById('dialogbox');
+      
+      let winH = window.innerHeight;
+      dialogoverlay.style.height = winH+"px";
+      
+      dialogbox.style.top = "100px";
+  
+      dialogoverlay.style.display = "block";
+      dialogbox.style.display = "block";
+      
+      document.getElementById('dialogboxhead').style.display = 'block';
+  
+      if(typeof title === 'undefined') {
+        document.getElementById('dialogboxhead').style.display = 'none';
+      } else {
+        document.getElementById('dialogboxhead').innerHTML = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> '+ title;
+      }
+      document.getElementById('dialogboxbody').innerHTML = message;
+      document.getElementById('dialogboxfoot').innerHTML = '<button class="pure-material-button-contained active" onclick="customAlert.ok()">OK</button>';
+    }
+    
+    this.ok = function(){
+      document.getElementById('dialogbox').style.display = "none";
+      document.getElementById('dialogoverlay').style.display = "none";
+    }
+  }
+  
+  let customAlert = new CustomAlert();
+
+
+function generatePassword() {
+    const length = parseInt(lenghtInput.value);
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset.charAt(randomIndex);
+    }
+    passwordDiv.textContent = password;
+}
+
+function copyPassword() {
+    const passwordText = passwordDiv.textContent;
+    if (passwordText) {
+        const tempInput = document.createElement('input');
+        tempInput.value = passwordText;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand('copy');
+        document.body.removeChild(tempInput);
+        alert("Password copied to clipboard!")
+    }
+}
+
+
